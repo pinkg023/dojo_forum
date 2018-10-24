1c98a287b526a4e57b162479b7512babe8f0bda8
pinkg023 <pinkg023@g.ncu.edu.tw>
2018-10-24 15:52:52 +0800

    add Feed link to application view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181022030734) do
+ActiveRecord::Schema.define(version: 20181024074643) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 20181022030734) do
   create_table "caterelates", force: :cascade do |t|
     t.integer "post_id"
     t.integer "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "collects", force: :cascade do |t|
+    t.integer "post_id"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
