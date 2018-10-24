e06b475b938f89b26f0e45dc4b91d416572c4773
pinkg023 <pinkg023@g.ncu.edu.tw>
2018-10-24 18:04:54 +0800

    add basic user_edit page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,4 +18,29 @@ class UsersController < ApplicationController
       #authorize! :read, @post
       return(@posts)
   end
+
+  def edit
+    set_user
+  end
+
+  def update
+    set_user
+    if @user.update(user_params)
+      flash[:notice] = "user was successfully updated"
+      redirect_to mypost_user_path(@user)      
+    else
+      render :edit
+      flash[:alert] = @user.errors.full_messages.to_sentence
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :opening_hours, :tel, :address, :description, :image, :category_id)
+  end
 end
