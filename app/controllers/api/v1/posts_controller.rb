53b33e08f7b340637fe0cb7ca4d121bf76716f30
pinkg023 <pinkg023@g.ncu.edu.tw>
2018-10-26 16:52:18 +0800

    finish api CRUD

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::PostsController < ApiController
   before_action :authenticate_user!, except: [:index]
-  
+
   def index
     @posts = Post.all
     render json: {
@@ -23,7 +23,7 @@ class Api::V1::PostsController < ApiController
   end
 
   def create
-    @post = Post.new(post_params)
+    @post = current_user.posts.new(post_params)
     if @post.save
       render json: {
         message: "Post created successfully!",
@@ -38,30 +38,42 @@ class Api::V1::PostsController < ApiController
 
   def update
     @post = Post.find_by(id: params[:id])
-    if @post.update(post_params)
+    if @post.user.authentication_token != params[:auth_token]
       render json: {
-        message: "Post updated successfully!",
-        result: @post
+        message: "You have no permittion!",
       }
     else
-      render json: {
-        errors: @post.errors
-      }
+      if @post.update(post_params)
+        render json: {
+          message: "Post updated successfully!",
+          result: @post
+        }
+      else
+        render json: {
+          errors: @post.errors
+        }
+      end
     end
   end
 
   def destroy
     @post = Post.find_by(id: params[:id])
-    @post.destroy
-    render json: {
-      message: "Post destroy successfully!"
-    }
+    if @post.user.authentication_token != params[:auth_token]
+      render json: {
+        message: "You have no permittion!",
+      }
+    else    
+      @post.destroy
+      render json: {
+        message: "Post destroy successfully!"
+      }
+    end
   end
 
   private
 
   def post_params
-    params.permit(:title, :description, :image, :user_id, :draft, :replies_count, :view_count)
+    params.permit(:title, :description, :user_id, :image, :draft, :replies_count, :view_count)
   end
 
 end
