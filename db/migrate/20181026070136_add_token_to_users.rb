85595f3e52317960e99e9e72be75589c1c2ce6f3
pinkg023 <pinkg023@g.ncu.edu.tw>
2018-10-26 17:38:53 +0800

    no auth-token at the begin

--- db/migrate/20181026070136_add_token_to_users.rb
+++ db/migrate/20181026070136_add_token_to_users.rb
@@ -3,10 +3,10 @@ class AddTokenToUsers < ActiveRecord::Migration[5.1]
     add_column :users, :authentication_token, :string
     add_index :users, :authentication_token, :unique => true
 
-    User.find_each do |user|
-      user.generate_authentication_token
-      user.save!
-    end
-    puts "generate user #{user.id} token"
+    #User.find_each do |user|
+    #  user.generate_authentication_token
+    #  user.save!
+    #end
+    #puts "generate user #{user.id} token"
   end
 end
