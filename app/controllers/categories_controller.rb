fa5c45935bd2dfffbbb6754a1b87ec8f712352c5
pinkg023 <pinkg023@g.ncu.edu.tw>
2018-11-19 20:21:35 +0800

    exculde draft from category pages

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,7 +4,7 @@ class CategoriesController < ApplicationController
   def show
       @categories = Category.all
       @category = Category.find(params[:id])
-      @posts = @category.posts.page(params[:page]).per(20)
+      @posts = @category.posts.where(draft: false).page(params[:page]).per(20)
       #@posts = Post.where(draft: false, category_id: params[:id]).page(params[:page]).per(20)
 
   end
@@ -12,21 +12,21 @@ class CategoriesController < ApplicationController
   def order_last_reply
     @categories = Category.all
     @category = Category.find(params[:id])
-    @posts = @category.posts.order(last_reply_time: :desc).page(params[:page]).per(20)      
+    @posts = @category.posts.where(draft: false).order(last_reply_time: :desc).page(params[:page]).per(20)      
     @user = current_user
   end
 
   def order_most_reply
     @categories = Category.all
     @category = Category.find(params[:id])
-    @posts = @category.posts.order(replies_count: :desc).page(params[:page]).per(20)
+    @posts = @category.posts.where(draft: false).order(replies_count: :desc).page(params[:page]).per(20)
     @user = current_user
   end
 
   def order_most_view
     @categories = Category.all
     @category = Category.find(params[:id])
-    @posts = @category.posts.order(views_count: :desc).page(params[:page]).per(20)
+    @posts = @category.posts.where(draft: false).order(views_count: :desc).page(params[:page]).per(20)
     @user = current_user
   end
 
